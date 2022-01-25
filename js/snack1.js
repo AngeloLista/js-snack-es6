13165bdf58a7c453953f3787e9d4aaff4d93c612
AngeloLista <abstractplayer@gmail.com>
2022-01-26 00:05:52 +0100

    found lightest bike (snack1)

--- js/snack1.js
+++ js/snack1.js
@@ -1,7 +1,7 @@
 /*
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-Stampare ain console la bici con peso minore utilizzando destructuring e template literal
+Stampare in console la bici con peso minore utilizzando destructuring e template literal
 */
 
 const bikes = [
@@ -15,6 +15,15 @@ const bikes = [
     },
     {
         nome: 'Marin',
-        weight: 15,
+        weight: 7,
     },
 ]
+
+lightestBike = bikes[0];
+
+for (let i = 0; i < bikes.length; i++) {
+    const bike = bikes[i];
+    const {weight} = bike;
+    if(bike.weight < lightestBike.weight) lightestBike = bike;
+}
+
