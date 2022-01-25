a72e615e20d3dc80af9abb1e0ef2c7aab631deaa
AngeloLista <abstractplayer@gmail.com>
2022-01-25 23:57:00 +0100

    created bikes array

---
+++ js/snack1.js
@@ -0,0 +1,20 @@
+/*
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare ain console la bici con peso minore utilizzando destructuring e template literal
+*/
+
+const bikes = [
+    {
+        nome: 'Atala',
+        weight: 9,
+    },
+    {
+        nome: 'Merida',
+        weight: 11,
+    },
+    {
+        nome: 'Marin',
+        weight: 15,
+    },
+]
